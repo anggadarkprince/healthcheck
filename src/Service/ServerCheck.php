6b3fab093ca7f1bb1fb3f576176451a249577de9
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-05-11 13:21:54 +0700

    fix directory summary

--- src/Service/ServerCheck.php
+++ src/Service/ServerCheck.php
@@ -46,12 +46,37 @@ class ServerCheck implements HealthCheck
                 if ($value < 1024) {
                     $disk[$key] = $value . ' B';
                 } elseif ($value < 1048576) {
-                    $disk[$key] = round($value / 1024, 2) . ' KB';
+                    $disk[$key] = round($value / 1024, 2) . 'KB';
                 } elseif ($value < 1048576000) {
-                    $disk[$key] = round($value / 1048576, 2) . ' MB';
+                    $disk[$key] = round($value / 1048576, 2) . 'MB';
                 } else {
-                    $disk[$key] = round($value / 1048576000, 2) . ' GB';
+                    $disk[$key] = round($value / 1048576000, 2) . 'GB';
                 }
+            }
+        }
+
+        $directoryReport = getenv('SERVER_DISK_DIRECTORY_SUMMARY');
+        if (!empty($directoryReport)) {
+            if (strpos($this->os, 'win') !== false) {
+                $disk['directory_report'] = [
+                    'path' => $directoryReport,
+                    'contents' => []
+                ];
+            } else {
+                $directoryReportScan = rtrim($directoryReport, "/") . '/*';
+                $data = explode("\n", shell_exec("du -hs {$directoryReportScan}"));
+                $result = [];
+                foreach ($data as $line) {
+                    if (!empty(trim($line))) {
+                        list($size, $path) = explode(" ", preg_replace('/\s+/', ' ', $line));
+                        $dir = substr($path, strlen($directoryReport));
+                        $result[trim($dir)] = trim($size);
+                    }
+                }
+                $disk['directory_report'] = [
+                    'path' => $directoryReport,
+                    'contents' => $result,
+                ];
             }
         }
 
@@ -71,9 +96,9 @@ class ServerCheck implements HealthCheck
                 if (strpos($row, 'Mem') !== false) {
                     $memories = explode(" ", preg_replace('/\s+/', ' ', $row));
                     $ram = [
-                        'total' => number_format($memories[1] / 1000, 2) . ' GB',
-                        'used' => number_format($memories[2] / 1000, 2) . ' GB',
-                        'available' => number_format(end($memories) / 1000, 2) . ' GB'
+                        'total' => number_format($memories[1] / 1000, 2) . 'GB',
+                        'used' => number_format($memories[2] / 1000, 2) . 'GB',
+                        'available' => number_format(end($memories) / 1000, 2) . 'GB'
                     ];
                 }
             }
